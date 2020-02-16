401cfd32c196700884c44d7b0a60bb0144808ed5
ryan-doughty <ryan@trdoughty.com>
2020-02-16 16:59:39 -0500

    Update bad machine js

--- js/bad-machine.js
+++ js/bad-machine.js
@@ -1,34 +1,34 @@
 var items = [
   {
     song: "Stay A While",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit%20-%20%20Stay%20a%20while%20(vodka%20boy).mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/stay-awhile.mp3"
   },
   {
     song: "Jitter, Twitch, and Shake",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit%20-%20Jitter,%20Twitch%20and%20Shake.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/jitter-twitch-and-shake.mp3"
   },
   {
     song: "Naysayer",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20the%20rabbit%20-%20%20Naysayer.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/naysayer.mp3"
   },
   {
     song: "New Relationship Basis",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit-%20New%20Relationship%20Basis.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/new-relationship-basis.mp3"
   },
   {
     song: "Judas Blues",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit%20-%20Judas%20Blues.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/judas-blues.mp3"
   },
   {
     song: "Cripple Creek",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit%20-%20Cripple%20Creek.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/cripple-creek.mp3"
   },
   {
     song: "Momentum",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit-%20Momentum.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/momentum.mp3"
   },
   {
     song: "Cast",
-    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/00%20The%20Rabbit%20-%20Cast.mp3"
+    trackURL: "http://trdoughty.com/00-the-rabbit/music/bad-machine/cast.mp3"
   }
 ];
