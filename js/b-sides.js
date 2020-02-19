606deee08c2ebd1ff53e1fd0b338df088e07f560
ryan-doughty <ryan@trdoughty.com>
2020-02-18 19:02:18 -0500

    Add B-sides

--- js/b-sides.js
+++ js/b-sides.js
@@ -1,22 +1,30 @@
 var items = [
 	{
-		song: "Friendly",
-		trackURL: "http://trdoughty.com/00-the-rabbit/music/bee-sides/friendly.mp3"
+		song: "17 (Live)",
+		trackURL: "music/bee-sides/17-live.mp3"
+	},
+	{
+		song: "Get Together/Be Afraid (Live)",
+		trackURL: "music/bee-sides/be-afraid-live.mp3"
 	},
 	{
 		song: "Jitter, Twitch, and Shake (Version 1)",
-		trackURL: "http://trdoughty.com/00-the-rabbit/music/bee-sides/jitter-twitch-and-shake.mp3"
+		trackURL: "music/bee-sides/jitter-twitch-and-shake.mp3"
 	},
 	{
 		song: "Little Rabbit",
-		trackURL: "http://trdoughty.com/00-the-rabbit/music/bee-sides/little-rabbit.mp3"
+		trackURL: "music/bee-sides/little-rabbit.mp3"
 	},
 	{
 		song: "Stealin&apos;",
-		trackURL: "http://trdoughty.com/00-the-rabbit/music/bee-sides/stealin.mp3"
+		trackURL: "music/bee-sides/stealin.mp3"
 	},
 	{
 		song: "France",
-		trackURL: "http://trdoughty.com/00-the-rabbit/music/bee-sides/france.mp3"
+		trackURL: "music/bee-sides/france.mp3"
+	}
+	{
+		song: "Sexy Back (Live)",
+		trackURL: "music/bee-sides/sexy-back.mp3"
 	}
 ];
